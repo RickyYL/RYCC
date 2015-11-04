7c136a7dbca01a11f4a1062b79217587968dd3db
Ricky Y. Lee <rickylee0914@gmail.com>
2015-11-03 23:10:39 -0800

    test cases update

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -13,15 +13,14 @@
 #include "Lexer.hpp"
 #include "Token.hpp"
 #include "Error.hpp"
-
-#include "BTParser.hpp"
+#include "MemBTParser.hpp"
+#include "AST.hpp"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     
     std::string input = "[e, f] = [g, h]";
-    std::string input2;
     
     cout << "Input text: " << input << endl << "Begin tokenizing..." << endl;
     ListLexer myLexer(input);
@@ -30,8 +29,25 @@ int main(int argc, const char * argv[]) {
     for (auto token: tokens)
         cout << "\t" << token.text() << " " << Token::getTokenName(token.type()) << endl;
     myLexer.reset();
+    cout << "Tokenizing succeed." << endl << endl;
     
-    MemParser myParser(myLexer);
+    cout << "Begin parsing..." << endl;
+    Parser myParser(myLexer);
+    myParser.stat();
+    cout << "Parsing succeed." << endl << endl;
+    
+    cout << "Begin building AST..." << endl;
+    Token plus = Token(42, "+");
+    Token one  = Token(42, "1");
+    Token two  = Token(42, "2");
+    AST root(plus);
+    root.addChild(new AST(one));
+    root.addChild(new AST(two));
+    cout << "\t" << root.toStringTree() << endl;
+    
+    AddNode root2(IntNode(two), plus ,IntNode(two));
+    cout << "\t" << root2.toStringTree() << endl;
+    cout << "Building AST succeed." << endl;
     
     return 0;
 }
